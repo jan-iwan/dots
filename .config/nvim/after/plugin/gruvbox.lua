a558bb97f75ea7da29821aea778285f605a4e442
jan-iwan <mainkarang@gmail.com>
2023-04-08 14:19:31 -0300

    neovim gruvbox

--- .config/nvim/after/plugin/gruvbox.lua
+++ .config/nvim/after/plugin/gruvbox.lua
@@ -10,17 +10,15 @@ end
 -- setup must be called before loading the colorscheme
 -- Default options:
 require("gruvbox").setup({
-    undercurl = undercurl,
+    -- undercurl = undercurl,
     underline = true,
     bold = true,
     italic = {
         strings = false,
         operators = false,
         comments = false,
-        folds = true,
-    },
-    strikethrough = true,
-    invert_selection = true,
+        folds = false,
+    }, strikethrough = true, invert_selection = true,
     invert_signs = false,
     invert_tabline = false,
     invert_intend_guides = false,
@@ -30,10 +28,18 @@ require("gruvbox").setup({
     overrides = {
         Comment = { fg = "#98971a" },
         String = { fg = "#d65d0e" },
-        Type = { fg = "#689d6a", bold = true },
+        Typedef = { fg = "#fabd2f", bold = false },
+        Type = { fg = "#458588", bold = true },
         Special = { fg = "#d79921" },
         Number = { fg = "#b8bb26" },
         Function = { fg = "#d79921", bold = true },
+        Macro = { fg = "#d3869b"},
+        Identifier = { fg = "#d5c4a1" },
+        Structure = { fg = "#458588" },
+        PreProc = { fg = "#689d6a" },
+        Include = { fg = "#689d6a" },
+        Define = { fg = "#689d6a" },
+
         -- i hate undercurl
         GruvboxRedUnderline = { underline = true, undercurl = false },
         GruvboxGreenUnderline = { underline = true, undercurl = false },
