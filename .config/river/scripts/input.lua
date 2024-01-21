3a107bf07470e094fe1821c08f07b8f0a0f8eda3
jan-iwan <mainkarang@gmail.com>
2024-01-21 13:26:19 -0300

    river config in lua

--- .config/river/scripts/input.lua
+++ .config/river/scripts/input.lua
@@ -3,16 +3,16 @@ local mouse = "pointer-1133-16500-Logitech_G305"
 
 local input_settings = {
     -- keyboard
-    ["set-repeat"] = "50 300",
-    ["keyboard-layout"] = "-variant altgr-intl -options nodeadkeys us",
+    "set-repeat 50 300",
+    "keyboard-layout -variant altgr-intl -options nodeadkeys us",
 
     -- mouse
-    ["input " .. mouse] = "accel-profile flat",
-    ["input " .. mouse] = "pointer-accel -0.5",
-    ["focus-follows-cursor"] = "disabled",
-    ["hide-cursor timeout"] = "27953",
+    "focus-follows-cursor disabled",
+    "hide-cursor timeout 27953",
+    "input " .. mouse .. " accel-profile flat",
+    "input " .. mouse .. " pointer-accel -0.5",
 }
 
-for key, value in pairs(input_settings) do
-    os.execute(string.format("riverctl %s %s", key, value))
+for _, setting in ipairs(input_settings) do
+    os.execute(string.format("riverctl %s", setting))
 end
