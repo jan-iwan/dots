350a316dd2e79fa91be9341466cd4b1202943836
jan-iwan <mainkarang@gmail.com>
2024-01-25 23:40:37 -0300

    palette

---
+++ .config/palette.lua
@@ -0,0 +1,40 @@
+local colors = {
+    { name = "bg",              code = "#1d2021" },
+    { name = "dim bg",          code = "#282828" },
+    { name = "bg alt",          code = "#32302f" },
+    { name = "bright bg",       code = "#504945" },
+
+    { name = "fg",              code = "#ebdbb2" },
+    { name = "bright fg",       code = "#fbf1c7" },
+    { name = "fg alt",          code = "#d5c4a1" },
+    { name = "dim fg",          code = "#bdae93" },
+
+    { name = "black",           code = "#263640" },
+    { name = "red",             code = "#cc241d" },
+    { name = "green",           code = "#98971a" },
+    { name = "yellow",          code = "#d79921" },
+    { name = "blue",            code = "#458588" },
+    { name = "magenta",         code = "#b16282" },
+    { name = "cyan",            code = "#689d6a" },
+    { name = "white",           code = "#a89984" },
+
+    { name = "bright black",    code = "#4a697d" },
+    { name = "bright red",      code = "#fb4934" },
+    { name = "bright green",    code = "#b8bb26" },
+    { name = "bright yellow",   code = "#fabd2f" },
+    { name = "bright blue",     code = "#83a598" },
+    { name = "bright magenta",  code = "#d3869b" },
+    { name = "bright cyan",     code = "#8ec07c" },
+    { name = "bright white",    code = "#f9f5d7" },
+
+    { name = "dim black",       code = "#3c3836" },
+    { name = "dim red",         code = "#9d0006" },
+    { name = "dim green",       code = "#79740e" },
+    { name = "dim yellow",      code = "#b57614" },
+    { name = "dim blue",        code = "#076678" },
+    { name = "dim magenta",     code = "#8f3f71" },
+    { name = "dim cyan",        code = "#327b58" },
+    { name = "dim white",       code = "#928374" },
+}
+
+return colors
